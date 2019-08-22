58c51561b2fcc996af8837692792574c8f35f38b
hiroshi_yoshida <u.dance.gm@gmail.com>
2019-08-23 01:26:47 +0900

    質問投稿画面を追加

--- php-book-app/app/src/Controller/QuestionsController.php
+++ php-book-app/app/src/Controller/QuestionsController.php
@@ -28,4 +28,28 @@ class QuestionsController extends AppController
     
     $this->set(compact('questions'));
   }
+
+  /**
+   * 質問投稿画面/質問投稿処理
+   * 
+   * @return \Cake\Http\Response|null 諮問投稿後に質問一覧画面へ遷移する
+   */
+  public function add()
+  {
+    $question = $this->Questions->newEntity();
+
+    if ($this->request->is('post')) {
+        $question  = $this->Questions->patchEntity($question, $this->request->getData());
+        $question->user_id = 1; //＠TODO ユーザー管理機能実装時に修正する
+
+        if ($this->Questions->save($question)) {
+            $this->Flash->success('質問を投稿しました');
+
+            return $this->redirect(['action' => 'index']);
+        }
+        $this->Flash->error('質問の投稿に失敗しました');
+    }
+
+    $this->set(compact('question'));
+  }
 }
