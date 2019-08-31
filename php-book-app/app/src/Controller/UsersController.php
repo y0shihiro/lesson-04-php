fa2f733a60ed568cb3668d277d5b74c6798df817
hiroshi_yoshida <u.dance.gm@gmail.com>
2019-08-31 19:04:42 +0900

    ユーザー登録画面の実装

---
+++ php-book-app/app/src/Controller/UsersController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+/**
+ * Users Controller
+ * 
+ * @property \APP\Model\Table\UsersTable $Users
+ */
+class UsersController extends AppController
+{
+    /**
+     * @inheritdoc
+     */
+    public function initialize()
+    {
+        parent::initialize();
+    }
+
+    /**
+     * ユーザー登録画面/ユーザー登録処理
+     * 
+     * @return \Cake\Http\Response|null ユーザー登録後にログイン画面へ遷移する
+     */
+    public function add()
+    {
+      $user = $this->Users->newEntity();
+      if ($this->request->is('post')) {
+        $user = $this->Users->patchEntity($user, $this->request->getData());
+        if ($this->Users->save($user)) {
+          $this->Flash->success('ユーザーの登録が完了しました');
+
+          return $this->redirect(['controller' => 'Login', 'action' => 'index']);
+        }
+        $this->Flash->error('ユーザーの登録に失敗しました');
+      }
+      $this->set(compact('user'));
+    }
+}
