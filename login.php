15366bc5082e04b63ac58425f3a09e6298e0116b
hiroshi_yoshida <u.dance.gm@gmail.com>
2019-09-07 17:49:03 +0900

    ログイン機能を追加

--- login.php
+++ login.php
@@ -1,3 +1,32 @@
+<?php
+require('dbconnect.php');
+
+session_start();
+
+if (!empty($_POST)) {
+  //ログイン処理
+  if ($_POST['email'] != '' && $_POST['password'] != '') {
+    $login = $db->prepare('SELECT * FROM members WHERE email=? AND password=?');
+    $login->execute(array(
+      $_POST['email'],
+      sha1($_POST['password'])
+    ));
+    $member = $login->fetch();
+
+    if ($member) {
+      //ログイン成功
+      $_SESSION['id'] = $member['id'];
+      $_SESSION['time'] = time();
+
+      header('Location: index.php'); exit();
+    } else {
+      $error['login'] = 'failed';
+    }
+  } else {
+    $error['login'] = 'blank';
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -25,11 +54,17 @@
         <dl>
           <dt>メールアドレス</dt>
           <dd>
-            <input type="text" name="email" size="35" maxlength="255">
+            <input type="text" name="email" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['email'], ENT_QUOTES); ?>" >
+            <?php if ($error['login']  == 'blank'): ?>
+            <p class="error">* メールアドレスとパスワードをご記入ください</p>
+            <?php endif; ?>
+            <?php if ($error['login'] == 'failed'): ?>
+            <p class="error">* ログインに失敗しました。正しくご記入ください。</p>
+            <?php endif; ?>
           </dd>
           <dt>パスワード</dt>
           <dd>
-            <input type="password" name="password" size="35" maxlength="255">
+            <input type="password" name="password" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['password'], ENT_QUOTES); ?>">
           </dd>
           <dt>ログイン情報の記録</dt>
           <dd>
