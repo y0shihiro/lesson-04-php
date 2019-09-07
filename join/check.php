9c46f3908f30f19f47ece37d5ce14de436d41427
hiroshi_yoshida <u.dance.gm@gmail.com>
2019-09-07 14:31:05 +0900

    データベースに登録

--- join/check.php
+++ join/check.php
@@ -1,8 +1,24 @@
 <?php
 session_start();
+require('../dbconnect.php');
 
 if (!isset($_SESSION['join'])) {
   header('Location: index.php');
+  exit();
+}
+
+if (!empty($_POST)) {
+  //登録処理をする
+  $statement = $db->prepare('INSERT INTO members SET name=?, email=?, password=?, picture=?, created=NOW()');
+  echo $ret = $statement->execute(array(
+    $_SESSION['join']['name'],
+    $_SESSION['join']['email'],
+    sha1($_SESSION['join']['password']),
+    $_SESSION['join']['image']
+  ));
+  unset($_SESSION['join']);
+
+  header('Location: thanks.php');
   exit();
 }
 ?>
@@ -25,6 +41,7 @@ if (!isset($_SESSION['join'])) {
     </div>
     <div id="content">
       <form action="" method="post">
+        <input type="hidden" name="action" value="submit">
         <dl>
           <dt>ニックネーム</dt>
           <dd>
