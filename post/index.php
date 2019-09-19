4a709a22b1993b3fca184b5acf42d39ae958ecc9
hiroshi_yoshida <u.dance.gm@gmail.com>
2019-09-20 00:55:19 +0900

    Chapter6-9

--- post/index.php
+++ post/index.php
@@ -76,16 +76,22 @@ if (isset($_REQUEST['res'])) {
       </form>
 
       <!-- メッセージを表示させる -->
-      <?php foreach ($posts as $post): ?>
-      <div class="msg">
-        <img src="../../member_picture/<?php echo htmlspecialchars($post['picture'], ENT_QUOTES); ?>" width="48" height="48" alt="<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>">
-        <p>
-          <?php echo htmlspecialchars($post['message'], ENT_QUOTES); ?>
-          <span class="name">（<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>）</span>
-        [<a href="index.php?res=<?php echo htmlspecialchars($post['id'], ENT_QUOTES); ?>">Re</a>]
-        </p>
-        <p class="day"><?php echo htmlspecialchars($post['created'], ENT_QUOTES); ?></p>
-      </div>
+      <?php foreach ($posts as $post) : ?>
+        <div class="msg">
+          <img src="../../member_picture/<?php echo htmlspecialchars($post['picture'], ENT_QUOTES); ?>" width="48" height="48" alt="<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>">
+          <p>
+            <?php echo htmlspecialchars($post['message'], ENT_QUOTES); ?>
+            <span class="name">（<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>）</span>
+            [<a href="index.php?res=<?php echo htmlspecialchars($post['id'], ENT_QUOTES); ?>">Re</a>]
+          </p>
+          <p class="day">
+            <a href="view.php?id=<?php echo htmlspecialchars($post['id'], ENT_QUOTES); ?>"><?php echo htmlspecialchars($post['created'], ENT_QUOTES); ?></a>
+            <?php if ($post['reply_post_id'] > 0) : ?>
+              <a href="view.php?id=<?php echo htmlspecialchars($post['reply_post_id'], ENT_QUOTES); ?>">返信元のメッセージ</a>
+            <?php endif; ?>
+          </p>
+
+        </div>
       <?php endforeach; ?>
     </div>
 
