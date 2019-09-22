ddb83b356ca1693aaf519b5f4fb2fddcbcce474e
hiroshi_yoshida <u.dance.gm@gmail.com>
2019-09-22 23:13:30 +0900

    Chapter5-6

--- memo/03/index.php
+++ memo/03/index.php
@@ -1,36 +1,43 @@
+<?php
+require('./dbconnect.php');
+
+$memos = $db->query('SELECT * FROM memos ORDER BY id DESC');
+?>
+
 <!doctype html>
 <html lang="ja">
+
 <head>
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="css/style.css">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/style.css">
 
-<title>よくわかるPHPの教科書</title>
+  <title>よくわかるPHPの教科書</title>
 </head>
+
 <body>
-<header>
-<h1 class="font-weight-normal">よくわかるPHPの教科書</h1>    
-</header>
+  <header>
+    <h1 class="font-weight-normal">よくわかるPHPの教科書</h1>
+  </header>
 
-<main>
-<h2>Practice</h2>
-<pre>
-<?php
-try {
-  $db = new PDO('mysql:dbname=mydb;host=localhost;charset=utf8', 'root', 'root');
-} catch (PDOException $e) {
-  echo 'DB接続エラー： ' . $e->getMessage();
-}
+  <main>
+    <h2>Practice</h2>
+    <article>
+      <?php while ($memo = $memos->fetch()) : ?>
+        <p>
+          <a href="memo.php?id=<?php print($memo['id']); ?>">
+          <?php print(mb_substr($memo['memo'], 0, 50)); ?>
+          <?php print((mb_strlen($memo['memo']) > 50 ? '...' : '')); ?>
+        </a>
+        </p>
+        <time><?php print($memo['created_at']); ?></time>
+        <hr>
+      <?php endwhile; ?>
+    </article>
+  </main>
+</body>
 
-$records = $db->query('SELECT * FROM my_items');
-while ($record = $records->fetch()) {
-  print($record['item_name'] . "\n");
-}
-?>
-</pre>
-</main>
-</body>    
 </html>
