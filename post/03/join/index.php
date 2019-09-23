4d2f87d554186965893f229837e41fecd527a2ac
hiroshi_yoshida <u.dance.gm@gmail.com>
2019-09-23 22:56:42 +0900

    会員登録用のプログラムを追加

--- post/03/join/index.php
+++ post/03/join/index.php
@@ -1,3 +1,41 @@
+<?php
+require('../function.php');
+session_start();
+
+//エラー項目の確認
+if (!empty($_POST)) {
+  if ($_POST['name'] == '') {
+    $error['name'] = 'blank';
+  }
+  if ($_POST['email'] == '') {
+    $error['email'] = 'blank';
+  }
+  if (strlen($_POST['password'] < 4)) {
+    $error['password'] = 'length';
+  }
+  if ($_POST['password'] == '') {
+    $error['password'] = 'blank';
+  }
+  $fileName = $_FILES['image']['name'];
+  if (!empty($fileName)) {
+    $ext = substr($fileName, -3);
+    if ($ext != 'jpg' && $ext != 'gif') {
+      $error['image'] = 'type';
+    }
+  }
+
+  //画像をアップロードする
+  if (empty($error)) {
+    $image = date('YmdHis') . $fileName;
+    move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/' . $image);
+
+    $_SESSION['join'] = $_POST;
+    $_SESSION['join']['image'] = $image;
+    header('Location: check.php');
+    exit();
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -21,19 +59,37 @@
         <dl>
           <dt>ニックネーム<span class="required">必須</span></dt>
           <dd>
-            <input type="text" name="name" size="35" maxlength="255">
+            <input type="text" name="name" size="35" maxlength="255" value="<?php h($_POST['name']); ?>">
+            <?php if ($error['name'] == 'blank') : ?>
+              <p class="error">* ニックネームを入力してください</p>
+            <?php endif; ?>
           </dd>
           <dt>メールアドレス<span class="required">必須</span></dt>
           <dd>
-            <input type="text" name="email" size="35" maxlength="255">
+            <input type="text" name="email" size="35" maxlength="255" value="<?php h($_POST['email']); ?>">
+            <?php if ($error['email'] == 'blank') : ?>
+              <p class="error">* メールアドレスを入力してください</p>
+            <?php endif; ?>
           </dd>
           <dt>パスワード<span class="required">必須</span></dt>
           <dd>
-            <input type="password" name="password" size="10" maxlength="20">
+            <input type="password" name="password" size="10" maxlength="20" value="<?php h($_POST['password']); ?>">
+            <?php if ($error['password'] == 'blank') : ?>
+              <p class="error">* パスワードを入力してください</p>
+            <?PHP endif; ?>
+            <?php if ($error['password'] == 'length') : ?>
+              <p class="error">* パスワードは４文字以上で入力してください</p>
+            <?php endif; ?>
           </dd>
           <dt>写真など</dt>
           <dd>
             <input type="file" name="image" size="35">
+            <?php if ($error['image'] == 'type') : ?>
+              <p class="error">* 写真などは「.gif」または「.jpg」の画像を指定してください</p>
+            <?php endif; ?>
+            <?php if (!empty($error)) : ?>
+              <p class="error">* 恐れ入りますが、画像を改めて指定してください</p>
+            <?php endif; ?>
           </dd>
         </dl>
         <div><input type="submit" value="入力内容を確認する"></div>
