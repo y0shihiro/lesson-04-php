71c6883111af6b2c1a3b02ea56555df75a0458c8
hiroshi_yoshida <u.dance.gm@gmail.com>
2019-09-28 16:47:27 +0900

    ページングを設置する

--- post/03/index.php
+++ post/03/index.php
@@ -37,7 +37,23 @@ if (!empty($_POST)) {
 }
 
 //投稿を取得する
-$posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+$page = $_REQUEST['page'];
+if ($page == '') {
+  $page = 1;
+}
+$page = max($page, 1);
+
+//最終ページを取得する
+$counts = $db->query('SELECT COUNT(*) AS cnt FROM posts');
+$cnt = $counts->fetch();
+$maxPage = ceil($cnt['cnt'] / 5);
+$page = min($page, $maxPage);
+
+$start = ($page - 1) * 5;
+
+$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC LIMIT ?, 5');
+$posts->bindParam(1, $start, PDO::PARAM_INT);
+$posts->execute();
 
 //返信の場合
 if (isset($_REQUEST['res'])) {
@@ -94,6 +110,31 @@ if (isset($_REQUEST['res'])) {
           </p>
         </div>
       <?php endforeach; ?>
+
+      <ul class="paging">
+        <?php
+        if ($page > 1) {
+          ?>
+          <li><a href="index.php?page=<?php print($page - 1); ?>">前のページへ</a></li>
+        <?php
+        } else {
+          ?>
+          <li>前のページへ</li>
+        <?php
+        }
+        ?>
+        <?php
+        if ($page < $maxPage) {
+          ?>
+          <li><a href="index.php?page=<?php print($page + 1); ?>">次のページへ</a></li>
+        <?php
+        } else {
+          ?>
+          <li>次のページへ</li>
+        <?php
+        }
+        ?>
+      </ul>
     </div>
 
   </div>
